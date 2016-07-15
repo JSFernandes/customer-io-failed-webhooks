f4945c76f461b6ed5324cf2c47e75c3e5fc0126a
JSFernandes <joao.fernandes@seedrs.com>
2016-07-15 13:40:18 +0100

    Receive posts with the event that we want.

--- lib/web.rb
+++ lib/web.rb
@@ -1,5 +1,20 @@
 require "sinatra"
+require "json"
 
 get "/" do
   "Hello, world"
+end
+
+post "/" do
+  begin
+    parsed_params = JSON.parse(request.body.read.to_s)
+    event_type = parsed_params["event_type"]
+    if event_type == "email_failed"
+      { notified: "true" }.to_json
+    else
+      status 200
+    end
+  rescue JSON::ParserError
+    status 400
+  end
 end
--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -8,8 +8,37 @@ describe "the sinatra app" do
     Sinatra::Application
   end
 
+  def post_as_json(path, params)
+    post path, params.to_json, "CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"
+  end
+
   it "says hello world" do
     get "/"
     expect(last_response.body).to eq("Hello, world")
+  end
+
+  context "when receiving a POST without JSON" do
+    it "responds with status ok" do
+      post "/", event_type: "email_drafted"
+      expect(last_response.status).to eq(400)
+    end
+  end
+
+  context "when receiving a POST with JSON with event_type different from email_failed" do
+    it "responds with status ok" do
+      params = { event_type: "email_drafted" }
+      post_as_json("/", params)
+      expect(last_response.status).to eq(200)
+    end
+  end
+
+  context "when receiving a POST with event_type email_failed" do
+    it "responds with JSON" do
+      params = { event_type: "email_failed" }
+      post_as_json("/", params)
+      response_body = JSON.parse(last_response.body)
+      expect(last_response.status).to eq(200)
+      expect(response_body).to eq("notified" => "true")
+    end
   end
 end
